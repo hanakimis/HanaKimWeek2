e73a19b2d10db98284b062d3c2872eafc89bec31
Hana Kim <hanakimis@gmail.com>
2014-09-18 00:11:41 -0700

    first commit for Hana Week 2 assignment

---
+++ Carousel/IntroViewController.swift
@@ -0,0 +1,37 @@
+//
+//  IntroViewController.swift
+//  Carousel
+//
+//  Created by Hana Kim on 9/18/14.
+//  Copyright (c) 2014 Hana Kim. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
